a7d48b3113e104307bef2eb8687d18fe95840e2e
노한서 <112401561+ignaciocha@users.noreply.github.com>
2023-01-31 17:52:42 +0900

    프로그래머스 배열의 평균값

--- src/배열의평균값.java
+++ src/배열의평균값.java
@@ -2,30 +2,21 @@ public class 배열의평균값 {
 
     public static void main(String[] args) {
 
-        int numbers[] = {3,4,5,6,7,8};
+        int numbers[] = {3, 4, 5, 6, 7, 8};
         double answer = solution(numbers);
         System.out.println(answer);
     }
 
     // numbers_len은 배열 numbers의 길이입니다.
     static double solution(int numbers[]) {
+        double answer = 0;
         int sum = 0;
-        int numbers_len = numbers.length;
 
-        for (int i=0; i<numbers_len; i++){
+        for(int i=0; i<numbers.length; i++){
             sum += numbers[i];
         }
 
-        int div = sum/numbers_len;
-        double div2 = 0.0;
-
-        if (div == 5 || div == 0) {
-            div2 = div*0.1;
-        }else{
-            div2 = div;
-        }
-
-        double answer = sum%numbers_len + div2;
+        answer = (double) sum/numbers.length;
 
         return answer;
     }
